d99869e8c716e6d505e7706116b52836ff3040b2
Matthew Royal <masyukun@gmail.com>
2022-10-28 11:51:11 -0700

    End of quarter

--- redis_healthcheck/src/redis_healthcheck/cli.py
+++ redis_healthcheck/src/redis_healthcheck/cli.py
@@ -1,6 +1,7 @@
 import json
 import pkg_resources
 import re
+from datetime import datetime
 
 from rich.console import Console
 from rich.table import Table
@@ -15,9 +16,11 @@ pluginSiteUrl = "https://TBD/"
 console = Console()
 SERVER_LIST = 'serverList.json'
 PLUGIN_ORDER = ['collect','detect','correct']
+CSV_CHECKS_FILENAME = 'check-report-{}.csv'.format(datetime.now().strftime("%Y-%d-%m-%H%M%S"))
 
 # The Context -- I feel conflicted using this # https://wiki.c2.com/?ContextObjectsAreEvil
 context = {}
+noCodeChecks = {}
 
 
 
@@ -114,18 +117,111 @@ def openServerList(ctx, param, filename):
     return _dict
 
 
+def openCheckFile(ctx, param, filenames):
+    """Use a JSON checkfile to drive constraint checking"""
+
+    checkfileSpecs = []
+
+    # Handle one or more filenames
+    fns = []
+    if (type(filenames) == type([]) or type(filenames) == type(())):
+      fns.extend(filenames)
+    else:
+      print(type(filenames))
+      fns.append(filenames)
+
+    for filename in filenames:
+      _stream = open(filename)
+      _dict = json.load(_stream)
+      _stream.close()
+      checkfileSpecs.append({"filename": filename, "checks": _dict})
+         
+    # print('DEBUG CHECKFILES = {}'.format(checkfileSpecs))
+    return checkfileSpecs
+
+
+# Recursively descend the library Dict to find the fieldname and check the constraint
+def checkConstraint(library, fieldname, constraint, value):
+  thepath = None
+  thevalue = None
+  fitsConstraint = None
+  
+  # print('ENTERED checkConstraint')
+  # print(library)
+
+  # Terminal condition
+  if fieldname in library:
+    # NOTE: Not using match case statement for greater Pythonic compatibility
+    if constraint.lower() in ["==","=","eq"]:
+      return [fieldname], library[fieldname], library[fieldname] == value
+    elif constraint.lower() in [">=",">==","ge"]:
+      return [fieldname], library[fieldname], library[fieldname] >= value
+    elif constraint.lower() in ["<=","<==","le"]:
+      return [fieldname], library[fieldname], library[fieldname] <= value
+    elif constraint.lower() in [">","lt"]:
+      return [fieldname], library[fieldname], library[fieldname] > value
+    elif constraint.lower() in ["<","gt"]:
+      return [fieldname], library[fieldname], library[fieldname] < value
+    elif constraint.lower() in ["!=","<>","ne"]:
+      return [fieldname], library[fieldname], library[fieldname] != value
+    else:
+      raise Exception('Unsupported operator: {}'.format(constraint))
+  else:
+
+    # Recurse down the remaining arrays/tuples and objects
+    for k in library.keys():
+
+      if type(library[k]) == type({}):
+        # print('-->Descending "{}" type {}'.format(k, type(library[k])))
+        thepath, thevalue, fitsConstraint = checkConstraint(library[k], fieldname, constraint, value)
+        # print('fitsConstraint = {}'.format(fitsConstraint))
+        
+        # If there was a result down there, pass it on
+        if fitsConstraint is not None:
+          # Reconstruct the path, bottom up
+          thepath.insert(0, k)
+          # print('  breaking!')
+          break
+      elif type(library[k]) == type([]) or type(library[k]) == type(()):
+        for i, item in enumerate(library[k]):
+          if type(item) == type({}):
+            # print('-->Descending "{}"[{}]'.format(k, i))
+            thepath, thevalue, fitsConstraint = checkConstraint(item, fieldname, constraint, value)
+            # print('fitsConstraint = {}'.format(fitsConstraint))
+
+            # If there was a result down there, pass it on
+            if fitsConstraint is not None:
+              # Reconstruct the path, bottom up
+              thepath.insert(0, "{}[{}]".format(k, i))
+              # print('  breaking!')
+              break
+        # If there was a result down there, pass it on
+        if fitsConstraint is not None:
+          # print('  breaking!')
+          break
+
+      # else:
+      #   print('Skipping "{}" type {}'.format(k, type(library[k])))
+
+  # print('Returning result = {}'.format(result))
+  return thepath, thevalue, fitsConstraint
+
+
 @click.group()
 @click.option('-s', '--serverList', type=click.Path(dir_okay=False), default=SERVER_LIST, help='JSON containing list of servers [{fqdn, username, password},{...},...] of each managed Redis Enterprise cluster.', multiple=False, callback=openServerList)
+@click.option('-c', '--checkfile', type=click.Path(dir_okay=False), required=False, help='JSON containing constraints to check against the configuration.', multiple=True, callback=openCheckFile)
 @click.pass_context
-def cli(ctx, serverlist):
+def cli(ctx, serverlist, checkfile):
   """Redis Enterprise Healthcheck Tool"""
   context["serverList"] = serverlist
+  noCodeChecks["checkfile"] = checkfile
   ctx.obj = context
 
 
 @cli.command()
+@click.option('-o', '--output', default=CSV_CHECKS_FILENAME, help='Filename for check report as CSV', multiple=False, required=False, show_default=True)
 @click.pass_obj
-def start(context):
+def start(context, output):
   """Run Redis Healthcheck from CLI."""
 
   print(" :hospital: Oh hey! Let's do a Redis Healthcheck.")
@@ -153,10 +249,65 @@ def start(context):
     for pluginType in PLUGIN_ORDER:
       print('\nRunning plugin type "{}"'.format(pluginType))
       for plugin in [p for p in pluginList if "metadata" in p and pluginType in p["metadata"]['type']]:
+
+        # TODO Make this less dangerous... provide a READONLY view of the context
         print('\nRunning plugin "{}"...'.format( plugin['name'] ))
         result = plugin['metadata'][pluginType](context)
-        # print(result)
 
+        # Save the result back as context
+        # TODO Make this less dangerous... in case plugins don't return the context
+        if result is not None:
+          context = result
+
+  # print('DEBUG CONTEXT = {}'.format(context))
+
+  # Run the Checkfiles
+  if noCodeChecks["checkfile"] is not None:
+    # csv = "Constraint source,Constraint field name,Constraint,Constraint value,Observed value,Passed"
+    structuredChecks = []
+    checkResults = []
+
+    # Read each checkfile, process each checkfile format
+    print("\n:clipboard: {} checkfile{} found".format(len(noCodeChecks["checkfile"]), "s" if len(noCodeChecks["checkfile"]) != 1 else ""))
+    for checkfile in noCodeChecks["checkfile"]:
+      print('  Processing checkfile "{}"...'.format(checkfile['filename']))
+      for checkname in checkfile['checks'].keys():
+        check = checkfile['checks'][checkname]
+        if 'constraint' in check:
+          structuredChecks.append({'checkfile': checkfile['filename'],'checkfield':checkname,'constraint':check['constraint'],'error':check['error'],'value':check['value'],'message':check['message']})
+        else:
+          # TODO
+          print('    "{}" <-- String constraint not yet implemented'.format(checkname))
+    
+    # Do the checks and store the results
+    print("\n:clipboard: Running checks...")
+    for check in structuredChecks:
+      check['checkResult'] = {}
+      check['checkResult']['path'], check['checkResult']['actualValue'], check['checkResult']['matchesConstraint'] = checkConstraint(context, check['checkfield'], check['constraint'], check['value'])
+      
+      # DEBUG screen output
+      print('"{}" {} {}'.format(check['checkfield'], check['constraint'], check['value']))
+      print('  {}'.format(":white_check_mark: matches at {}".format("/".join(check['checkResult']['path'])) if check['checkResult']['matchesConstraint'] 
+        else ":warning: field not found" if check['checkResult']['matchesConstraint'] is None 
+        else ":x: constraint failed; saw = {} but expected {} {} at {}".format(check['checkResult']['actualValue'], check['constraint'], check['value'], "/".join(check['checkResult']['path']))))
+
+      checkResults.append(check)
+    
+    # Output the results of the checks to a report file
+    print('\n:clipboard: Writing CSV report file to "{}"'.format(output))
+    csv = "Constraint source,Constraint field name,Constraint,Constraint value,Observed value,Passed"
+    for check in checkResults:
+      csv += '\n{},{},{},{},{},{}'.format(
+        check['checkfile'],
+        check['checkfield'], check['constraint'], check['value'],
+        check['checkResult']['actualValue'], check['checkResult']['matchesConstraint']
+      )
+    reportOutput = open(output, "w")
+    reportOutput.write(csv)
+    reportOutput.close()
+
+  else:
+    print("\n:no_entry_sign: No checkfiles specified")
 
 
 
--- reht_plugin_nodestatus/setup.py
+++ reht_plugin_nodestatus/setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
 setup(
-    name="RE Healthcheck Plugin - Node Status",
-    version="0.0.1",
-    description="Check the nodes status - a plugin for RE Healthcheck",
+    name="RE Healthcheck Plugin - My Plugin",
+    version="1.0.1",
+    description="What my plugin does - a plugin for RE Healthcheck",
     author="Matthew Royal",
     author_email="matthew.royal@redis.com",
     url="https://github.com/masyukun",
     packages=[
         "redis_healthcheck",
         "redis_healthcheck.plugins",
-        "redis_healthcheck.plugins.checknodes",
+        "redis_healthcheck.plugins.my_plugin",
     ],
     package_dir={"": "src"},
     install_requires=[
